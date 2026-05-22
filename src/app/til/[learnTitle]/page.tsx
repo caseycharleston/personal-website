3af63153eaefef06830066d1cebec7b23f46ea7f
Casey Charleston <caseycharleston@gmail.com>
2026-05-22 08:49:39 -0500

    start on TIL page

---
+++ src/app/til/[learnTitle]/page.tsx
@@ -0,0 +1,9 @@
+import Header from '@/components/Header';
+
+export default function TILPage() {
+  return (
+    <main className="bg-[#FEFCF0] text-black">
+      <Header />
+    </main>
+  );
+}
---
+++ src/app/til/page.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+
+export default function TILsPage() {
+  return (
+    <main className="bg-[#FEFCF0] text-black">
+      <Header />
+
+    </main>
+  );
+}
